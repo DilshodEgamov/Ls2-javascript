9b6e9391bd63c669151b1bec651b47be230a4a67
Dilshod <dilshodegamov777@gmail.com>
2022-05-17 18:00:36 +0500

    JSdagi birinchi uuyga vazifa

---
+++ main.js
@@ -0,0 +1,29 @@
+var ism = prompt("Ismingizni kiriting...")
+var yosh = +prompt("Yoshingizni kiriting...")
+alert ("Ismingiz  " + ism + ", yoshingiz esa " + yosh + "da ekan.Tanishganimdan xursandman")
+
+alert("Matematik amallarga xush kelibsiz")
+alert("Bu yerda qo'shish(+) amalini bajarishingiz mumkin")
+var a = +prompt("Birinchi sonni kiriting")
+var b = +prompt("ikkinchi sonni kiriting")
+alert("Javob: "  + (a+b))
+
+alert("Bu yerda esa ayirish amalini bajarishingiz mumkin")
+var a = +prompt("Birinchi sonni kiriting")
+var b = +prompt("ikkinchi sonni kiriting")
+alert("Javob: "  + (a-b))
+
+alert("Bu yerda ko'paytirish amalini bajaramiz")
+var a = +prompt("Birinchi sonni kiriting")
+var b = +prompt("ikkinchi sonni kiriting")
+alert("Javob: "  + (a*b))
+
+alert("Bo'lish amalini bajarishga xush kelibsiz")
+var a = +prompt("Birinchi sonni kiriting")
+var b = +prompt("ikkinchi sonni kiriting")
+alert("Javob: "  + (a/b))
+
+alert("Bu yerda sonlarni foyizini hisoblaymiz")
+var a = +prompt("Birinchi sonni kiriting")
+var b = +prompt("ikkinchi sonni kiriting")
+alert("Javob: "  + (a%b))
